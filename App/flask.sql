6d58040bb0581cd2565ad1a45baaf8f727d52c35
Nawawi Palawan <126594145+Wawieee@users.noreply.github.com>
2023-10-20 19:16:43 +0800

    Update flask.sql

--- App/flask.sql
+++ App/flask.sql
@@ -1,24 +1,19 @@
-DROP DATABASE IF EXISTS ssiswebapp;
-CREATE DATABASE IF NOT EXISTS ssiswebapp;
-
+CREATE DATABASE ssiswebapp;
 USE ssiswebapp;
 
-DROP TABLE IF EXISTS college;
-CREATE TABLE IF NOT EXISTS college (
+CREATE TABLE college (
     code VARCHAR(10) PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS course;
-CREATE TABLE IF NOT EXISTS course (
+CREATE TABLE course (
     code VARCHAR(10) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     college_code VARCHAR(10),
     FOREIGN KEY (college_code) REFERENCES college(code)
 );
 
-DROP TABLE IF EXISTS student;
-CREATE TABLE IF NOT EXISTS student (
+CREATE TABLE student (
     id CHAR(9) PRIMARY KEY,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
@@ -27,3 +22,22 @@ CREATE TABLE IF NOT EXISTS student (
     gender ENUM('Male', 'Female', 'Other') NOT NULL,
     FOREIGN KEY (course_code) REFERENCES course(code)
 );
+
+DELIMITER //
+CREATE TRIGGER before_student_insert
+BEFORE INSERT ON student
+FOR EACH ROW
+BEGIN
+    DECLARE current_year INT;
+    DECLARE next_student_id INT;
+
+    SET current_year = YEAR(NOW());
+
+    SELECT IFNULL(MAX(CONVERT(SUBSTRING(id, 6), SIGNED)), 0) + 1 INTO next_student_id
+    FROM student
+    WHERE SUBSTRING(id, 1, 4) = current_year;
+
+    SET NEW.id = CONCAT(current_year, '-', LPAD(next_student_id, 4, '0'));
+END;
+//
+DELIMITER ;
