c416a125c08a57d38260115b38866a52b4221ffb
Nawawi Palawan <126594145+Wawieee@users.noreply.github.com>
2023-11-14 21:06:31 +0800

    Update course.py

--- app/models/course.py
+++ app/models/course.py
@@ -9,7 +9,8 @@ def get_courses():
 
 def add_course(code, name, college_code):
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO course (code, name, college_code) VALUES (%s, %s, %s)", (code, name, college_code))
+    cur.execute("INSERT INTO course (code, name, college_code) VALUES (%s, %s, %s)",
+                (code, name, college_code))
     mysql.connection.commit()
     cur.close()
 
@@ -20,11 +21,18 @@ def get_course_by_code(course_code):
     cur.close()
     return course
 
-def update_course(course_code, name, college_code):
+def update_course(old_course_code, new_course_code, name, college_code):
     cur = mysql.connection.cursor()
-    cur.execute("UPDATE course SET name = %s, college_code = %s WHERE code = %s", (name, college_code, course_code))
+    cur.execute("UPDATE course SET code = %s, name = %s, college_code = %s WHERE code = %s", (new_course_code, name, college_code, old_course_code))
     mysql.connection.commit()
     cur.close()
+
+def check_course_code_exists(course_code):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT COUNT(*) FROM course WHERE code = %s", (course_code,))
+    count = cur.fetchone()[0]
+    cur.close()
+    return count > 0
 
 def delete_course(code):
     cur = mysql.connection.cursor()
@@ -40,6 +48,13 @@ def search_courses(search_by, search_term):
         cur.execute("SELECT * FROM course WHERE name LIKE %s", ('%' + search_term + '%',))
     elif search_by == 'college_code':
         cur.execute("SELECT * FROM course WHERE college_code = %s", (search_term,))
+    courses = cur.fetchall()
+    cur.close()
+    return courses
+
+def get_courses_with_college():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT course.code, course.name, course.college_code, college.name FROM course JOIN college ON course.college_code = college.code")
     courses = cur.fetchall()
     cur.close()
     return courses
