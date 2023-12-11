202c6743d9e9f8eff80a90205d458556df95b5ee
Nawawi Palawan <126594145+Wawieee@users.noreply.github.com>
2023-12-11 20:49:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-MYSQL_HOST = 'localhost' 
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'nawawi'
-MYSQL_DB = 'ssiswebapp'
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
